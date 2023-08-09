f42f1812d00385721e393d5dbdae3ac1e7844722
csuka1219 <csukabalazs1219@gmail.com>
2023-08-09 13:50:59 +0200

    Update image-puller.py

--- image-puller.py
+++ image-puller.py
@@ -2,8 +2,8 @@ import os
 from ppadb.client import Client as AdbClient
 
 # Define supported image and video file extensions
-IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
-VIDEO_EXTENSIONS = ['.mp4', '.avi', '.mkv', '.mov']
+IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', 'webp']
+VIDEO_EXTENSIONS = ['.mp4', '.avi', '.mkv', '.mov' '.gif']
 
 def run_adb_shell(device, command):
     # Execute a shell command on the connected Android device using ADB
@@ -23,26 +23,31 @@ def pull_pictures_from_dir(device, remote_dir):
             # If the file has a supported image or video extension, pull it from the Android device to the local machine
             device.pull(remote_path, local_path)
 
-def get_folders_in_dcim(device):
-    dcim_dir = "/storage/emulated/0/DCIM"
+def get_folders(device, folder):
+    dir = f"/storage/emulated/0/{folder}"
     
-    # Check if the DCIM directory exists on the Android device
-    result = run_adb_shell(device, f'ls "{dcim_dir}"')
+    # Check if the directory exists on the Android device
+    result = run_adb_shell(device, f'ls "{dir}"')
     if "No such file or directory" in result:
         print("DCIM directory not found on the device.")
         return []
     
-    # List directories inside DCIM
-    result = run_adb_shell(device, f'find "{dcim_dir}" -mindepth 1 -maxdepth 1 -type d')
+    # List directories
+    result = run_adb_shell(device, f'find "{dir}" -mindepth 1 -maxdepth 1 -type d')
     folders = [folder.strip() for folder in result.splitlines()]
+    folders.append(dir)
     
     return folders
 
 def get_pictures_from_android():
     # Connect to ADB and get the device list
     client = AdbClient(host="127.0.0.1", port=5037)
-    devices = client.devices()
-    
+    try:
+        devices = client.devices()
+    except:
+        print("Adb connection error.")
+        return
+
     if not devices:
         print("No Android device found.")
         return
@@ -54,8 +59,9 @@ def get_pictures_from_android():
     if not os.path.exists("Android_Pictures"):
         os.makedirs("Android_Pictures")
 
-    # Get the list of subdirectories inside the DCIM directory on the Android device
-    folders = get_folders_in_dcim(device)
+    # Get the list of subdirectories inside the DCIM and Download directory on the Android device (you can extend it with another folders)
+    folders = get_folders(device, "DCIM")
+    folders.extend(get_folders(device, "Download"))
 
     # Pull pictures and videos from each subdirectory
     for folder in folders:
